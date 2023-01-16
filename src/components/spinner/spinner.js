76af5933ebd63d746f92511f4d104e38f5390851
Alyaev_Ivan <iv151@mail.ru>
2023-01-16 16:14:31 +0300

    random character func

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-eclipse-gpjhibtmgj"><div class="ldio-gn87q3n5ljd">
+<div></div>
+</div></div>
+    )
+}
+
+export default Spinner
